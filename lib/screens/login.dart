7eb51e52c0b459190163684a56d00b7447546c39
SandeshBhat <bhatsandesh15@gmail.com>
2021-09-01 15:27:04 +0530

    Login Screen and Welcome Screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        child: Image.asset('assets/logos/logo.png'),
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+          ],
+        ),
+      ),
+    );
+  }
+}
